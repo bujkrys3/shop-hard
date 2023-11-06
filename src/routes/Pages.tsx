e91fae44241b9e05fdc4fecf7c3b3868bee311d1
Krystian Bujak <bujakkrystian3@gmail.com>
2023-11-06 08:43:44 +0100

    06-11-23

--- src/routes/Pages.tsx
+++ src/routes/Pages.tsx
@@ -3,12 +3,12 @@ import { HomePage } from "../views/Home/HomePage";
 import { ProductPage } from "../views/ProductPage/ProductPage";
 import { Category } from "../views/Category/Category";
 import { Cart } from "../views/Cart/Cart";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, HashRouter } from "react-router-dom";
 import { DeliveryForm } from "../views/DeliveryForm/DeliveryForm";
 
 export const Pages = () => {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products/:id" element={<ProductPage />} />
@@ -16,6 +16,6 @@ export const Pages = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="/delivery" element={<DeliveryForm />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
