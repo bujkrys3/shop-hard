43732b1367b9a6e75201ec5b0bf925e5d97c2290
Krystian Bujak <bujakkrystian3@gmail.com>
2023-12-03 07:57:42 +0100

    1

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,41 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 import classes from "./Footer.module.scss";
 
 export const Footer = () => {
+  const openInNewTab = (url: string) => {
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+    if (newWindow) newWindow.opener = null;
+  };
+
   return (
     <div className={classes.footer}>
-      <FontAwesomeIcon icon={faFacebook} />
+      <FontAwesomeIcon
+        onClick={() => {
+          openInNewTab("https://www.facebook.com/");
+        }}
+        className={classes.footer__icon}
+        icon={faFacebook}
+      />
+      <FontAwesomeIcon
+        onClick={() => {
+          openInNewTab("https://twitter.com/?lang=pl");
+        }}
+        className={classes.footer__icon}
+        icon={faTwitter}
+      />
+      <FontAwesomeIcon
+        onClick={() => {
+          openInNewTab("https://www.instagram.com/");
+        }}
+        className={classes.footer__icon}
+        icon={faInstagram}
+      />
     </div>
   );
 };
